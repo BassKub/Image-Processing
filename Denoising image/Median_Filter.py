d26449e2c64de13f1f5029e6a8aaa8c391abb91d
Suparoek Grudsorn <87462178+BassKub@users.noreply.github.com>
2023-07-18 22:46:40 +0700

    make pic to reference

--- Denoising image/Median_Filter.py
+++ Denoising image/Median_Filter.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 img = cv.imread('Denoising image/01 with sp.png',cv.IMREAD_GRAYSCALE)
 
-output = cv.medianBlur(img,5)
+output = cv.medianBlur(img, 1)
 
 cv.imwrite('Input.png', img)
 cv.imwrite('Output.png', output)
